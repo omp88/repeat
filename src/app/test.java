b55f3438f8eb58ae42a2e8ac7d340f6e96cca86a
NikitaZayats <nikitaza0@gmail.com>
2022-09-06 15:10:37 +0300

    add plus msg

--- src/app/test.java
+++ src/app/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("start");
         System.out.println("dev changes");
+        System.out.println("add + mes");
     }
 }
